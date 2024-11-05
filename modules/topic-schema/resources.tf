36add0281fa029894b8e7735e7ad4a9f9d534c79
Ian Knighton <iknighton@deseretdigital.com>
2024-11-05 10:57:21 -0700

    chore: fix schema

--- modules/topic-schema/resources.tf
+++ modules/topic-schema/resources.tf
@@ -2,5 +2,10 @@ resource "google_pubsub_topic" "topic" {
   name   = var.topic_name
   labels = var.labels
 
+  schema_settings {
+    schema   = var.schema
+    encoding = var.schema_encoding
+  }
+
   message_retention_duration = var.message_retention_duration
 }
